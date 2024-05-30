89b576d5af611bc87ca9e193ecdbd8b8777864da
ChetinSergey <167962926+ChetinSergey@users.noreply.github.com>
2024-05-30 16:42:32 +0500

    Блокировка потоков для доступа к общим данным

---
+++ M10_03-Threads_Locks.py
@@ -0,0 +1,41 @@
+from threading import Thread, Lock
+
+lock = Lock()
+
+
+class BankAccount:
+
+    def __init__(self, balance):
+        self.balance = balance
+
+    def deposit(self, amount):
+        with lock:
+            self.balance += amount
+            print(f'Deposited {amount}, new balance is {self.balance}')
+
+    def withdraw(self, amount):
+        with lock:
+            self.balance -= amount
+            print(f'Withdrew {amount}, new balance is {self.balance}')
+
+
+def deposit_task(account, amount):
+    for _ in range(5):
+        account.deposit(amount)
+
+
+def withdraw_task(account, amount):
+    for _ in range(5):
+        account.withdraw(amount)
+
+
+account = BankAccount(1000)
+
+deposit_thread = Thread(target=deposit_task, args=(account, 100))
+withdraw_thread = Thread(target=withdraw_task, args=(account, 150))
+
+deposit_thread.start()
+withdraw_thread.start()
+
+deposit_thread.join()
+withdraw_thread.join()
